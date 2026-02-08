f5763b802e65d942bfb2768a797b2f9e39fd788e
James Brierley <jmb8710@gmail.com>
2026-02-08 11:37:31 +0000

    add CharacterRenderer

---
+++ app/src/main/kotlin/com/github/jimbovm/sofia/presenter/editor/CharacterRenderer.kt
@@ -0,0 +1,96 @@
+package com.github.jimbovm.sofia.presenter.editor
+
+import javafx.scene.canvas.Canvas
+import javafx.scene.image.Image
+
+import com.github.jimbovm.isobel.common.Area
+import com.github.jimbovm.isobel.actor.population.PopulationActor
+import com.github.jimbovm.isobel.actor.population.Character
+
+import com.github.jimbovm.sofia.presenter.editor.Sprite
+
+class CharacterRenderer : Renderer {
+	
+	constructor(canvas: Canvas, area: Area) : super(canvas, area)
+
+	public override fun render(): Unit {
+
+		for (populationActor in this.area.population) {
+			when (populationActor) {
+				is Character -> {
+					val characterActor = populationActor
+					when (characterActor.type) {
+						Character.Type.BLOOBER,
+						Character.Type.BOWSER,
+						Character.Type.BUZZY_BEETLE,
+						Character.Type.GOOMBA,
+						Character.Type.GREEN_CHEEP,
+						Character.Type.GREEN_PARATROOPA_HOP,
+						Character.Type.GREEN_PARATROOPA_HOVER,
+						Character.Type.GREEN_TROOPA,
+						Character.Type.HAMMER_BRO,
+						Character.Type.LAKITU,
+						Character.Type.PODOBOO,
+						Character.Type.RED_CHEEP,
+						Character.Type.RED_PARATROOPA,
+						Character.Type.RED_TROOPA,
+						Character.Type.SPINY,
+						Character.Type.TOAD_PEACH -> {
+							this.renderSingleton(characterActor)
+						}
+						else -> {
+							if (characterActor.type.toString().contains("SQUAD", false)) {
+								this.renderSquad(characterActor);
+							}
+							else if (characterActor.type.toString().contains("FIRE_BAR", false)) {
+								this.renderFireBar(characterActor)
+							}
+							else if (characterActor.type.toString().contains("LIFT", false)) {
+								this.renderLift(characterActor)
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
+	private fun renderSingleton(characterActor: Character): Unit {
+
+		val characterName = characterActor.type.toString()
+		val characterMetatile = Sprite.Metatile.valueOf(characterName)
+		this.drawSprite(characterMetatile.sprite, characterActor.x, characterActor.y)
+	}
+
+	private fun renderSquad(squad: Character): Unit {
+
+		val type = squad.type.toString()
+
+		val metatile = if (type.startsWith("GOOMBA")) Sprite.Metatile.GOOMBA.sprite else Sprite.Metatile.GREEN_TROOPA.sprite
+		val members = if (type.contains("3", false)) 3 else 2
+		val y = if (type.contains("Y10", false)) 10 else 6
+
+		for (member in 0..<members) {
+			drawSprite(metatile, squad.x, y, member * 23.0, 0.0)
+		}
+	}
+
+	private fun renderFireBar(fireBar: Character): Unit {
+
+	}
+
+	private fun renderLift(lift: Character): Unit {
+
+		val liftName = lift.toString()
+		val liftType = liftName.toString().substring(5 + liftName.lastIndexOf("LIFT_"))
+		val isShort = liftName.startsWith("SHORT")
+
+		val extent = if (isShort) 1 else 2
+
+		for (x in (lift.x)..(lift.x+extent)) {
+
+			this.drawSprite(Sprite.Metatile.LIFT.sprite, x, lift.y)
+			this.drawSprite(Sprite.Metatile.LIFT.sprite, x, lift.y, 8.0, 0.0)
+		}
+	}
+}
--- app/src/main/kotlin/com/github/jimbovm/sofia/view/Main.kt
+++ app/src/main/kotlin/com/github/jimbovm/sofia/view/Main.kt
@@ -32,11 +32,17 @@ import java.util.*
 import com.github.jimbovm.isobel.actor.geography.BackgroundModifier
 import com.github.jimbovm.isobel.actor.geography.ExtensiblePlatform
 import com.github.jimbovm.isobel.actor.geography.FillSceneryModifier
+import com.github.jimbovm.isobel.actor.geography.FixedStatic
+import com.github.jimbovm.isobel.actor.geography.SingletonObject
+import com.github.jimbovm.isobel.actor.population.Character
 import com.github.jimbovm.isobel.common.Area
 import com.github.jimbovm.isobel.common.AreaHeader
 import com.github.jimbovm.isobel.common.AreaHeader.Background
 import com.github.jimbovm.sofia.presenter.editor.BackgroundFillSceneryRenderer
+import com.github.jimbovm.sofia.presenter.editor.CharacterRenderer
 import com.github.jimbovm.sofia.presenter.editor.ExtensiblePlatformRenderer
+import com.github.jimbovm.sofia.presenter.editor.FixedStaticRenderer
+import com.github.jimbovm.sofia.presenter.editor.SingletonObjectRenderer
 import com.github.jimbovm.sofia.presenter.editor.StartPositionRenderer
 import com.github.jimbovm.sofia.presenter.editor.TextRenderer
 
@@ -58,7 +64,7 @@ class Main() : Application() {
 
 		val area = Area().apply {
 			header = AreaHeader().apply {
-				background = Background.NIGHT_SNOW
+				background = Background.NONE
 				fill = AreaHeader.Fill.FILL_2BF_0BC
 				platform = AreaHeader.Platform.TREE
 				scenery = AreaHeader.Scenery.CLOUDS
@@ -67,6 +73,17 @@ class Main() : Application() {
 			}
 			environment = Area.Environment.OVERWORLD
 			geography = listOf(
+				SingletonObject.create(1, 1, SingletonObject.Type.BRICK_VINE),
+				SingletonObject.create(1, 2, SingletonObject.Type.QUESTION_BLOCK_COIN),
+				SingletonObject.create(1, 3, SingletonObject.Type.BRICK_MULTI_COIN),
+				SingletonObject.create(1, 4, SingletonObject.Type.BRICK_POWERUP),
+				SingletonObject.create(1, 5, SingletonObject.Type.BRICK_STARMAN),
+				SingletonObject.create(1, 6, SingletonObject.Type.HIDDEN_BLOCK_1UP),
+				SingletonObject.create(1, 7, SingletonObject.Type.HIDDEN_BLOCK_COIN),
+				SingletonObject.create(1, 8, SingletonObject.Type.QUESTION_BLOCK_USED),
+				SingletonObject.create(1, 9, SingletonObject.Type.BRICK_1UP),
+				SingletonObject.create(3, 1, SingletonObject.Type.SIDEWAYS_PIPE),
+				SingletonObject.create(4, 1, SingletonObject.Type.JUMPING_BOARD),
 				FillSceneryModifier.create(12, AreaHeader.Fill.FILL_NONE, AreaHeader.Scenery.CLOUDS),
 				BackgroundModifier.create(12, Background.OVER_WATER),
 				FillSceneryModifier.create(14, AreaHeader.Fill.FILL_2BF_0BC, AreaHeader.Scenery.CLOUDS),
@@ -79,8 +96,24 @@ class Main() : Application() {
 				FillSceneryModifier.create(59, AreaHeader.Fill.FILL_2BF_0BC, AreaHeader.Scenery.FENCES),
 				BackgroundModifier.create(61, Background.OVER_WATER),
 				BackgroundModifier.create(63, Background.NONE),
+				FixedStatic.create(63, FixedStatic.Type.BOWSER_BRIDGE),
+				FixedStatic.create(75, FixedStatic.Type.CHAIN),
+				FixedStatic.create(76, FixedStatic.Type.AXE),
 				ExtensiblePlatform.create(64, 9, 3),
-				FillSceneryModifier.create(81, AreaHeader.Fill.FILL_2BF_0BC, AreaHeader.Scenery.FENCES)
+				FillSceneryModifier.create(81, AreaHeader.Fill.FILL_2BF_0BC, AreaHeader.Scenery.FENCES),
+				FixedStatic.create(83, FixedStatic.Type.FLAGPOLE),
+				FixedStatic.create(86, FixedStatic.Type.TEE_PIPE),
+			)
+			population = listOf(
+				Character.create(7, 12, Character.Type.GOOMBA, false),
+				Character.create(12, 6, Character.Type.PODOBOO, false),
+				Character.create(13, 4, Character.Type.PODOBOO, false),
+				Character.create(19, 12, Character.Type.BOWSER, false),
+				Character.create(26, 8, Character.Type.HAMMER_BRO, false),
+				Character.create(36, 8, Character.Type.LIFT_UP_AND_DOWN, false),
+				Character.create(48, 0, Character.Type.TROOPA_SQUAD_2_Y6, false),
+				Character.create(48, 0, Character.Type.TROOPA_SQUAD_3_Y10, false),
+				
 			)
 		}
 
@@ -88,9 +121,15 @@ class Main() : Application() {
 		val startPositionRenderer = StartPositionRenderer(canvas, area)
 		val textRenderer = TextRenderer(canvas, area)
 		val extensiblePlatformRenderer = ExtensiblePlatformRenderer(canvas, area)
+		val characterRenderer = CharacterRenderer(canvas, area)
+		val singletonObjectRenderer = SingletonObjectRenderer(canvas, area)
+		val fixedStaticRenderer = FixedStaticRenderer(canvas, area)
 		backgroundFillSceneryRenderer.render()
 		startPositionRenderer.render()
 		extensiblePlatformRenderer.render()
+		fixedStaticRenderer.render()
+		characterRenderer.render()
+		singletonObjectRenderer.render()
 		textRenderer.apply {
 			xOrigin = 216.0
 			yOrigin = 16.0
